1eb3c45bbeec9cc9acf89b7f7365caa5c2b4a640
jokerthe54 <113138859+Jokerthe54@users.noreply.github.com>
2024-02-23 12:57:56 -0600

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,72 @@
-
+function Start () {
+    scene.setBackgroundImage(img`
+        fffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+        ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+        bbbbbbbbbbbbbbbbbbbbbbbbbbbffffbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+        ccccccccccccccccccccccccccfcccffffccccccccccccccccccccccccc
+        bbbbbbbbbbbbbbbbbbbbbbbbfffbbbbbbfbbbbbbbbbbbbbbbbbbbbbbbbb
+        ccccccccccccccccccccccffcccccfcffcccccccccccccccccccccccccc
+        bbbbbbbbbbbbbbbbbbbbbbfbbbbbbbfbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+        cccccccccccccccccccccfccccccccfcccccccccccccccccccccccccccc
+        bbbbbbbbbbbbbbbbbbbbbfbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+        ccccccccccccccccccccfcccccccccccccccccccccccccccccccccccccc
+        bbbbbbbbbbbbbbbbbbb2fbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+        cccccccccccccccccccfccccccccccccccccccccccccccccccccccccccc
+        bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+        ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+        bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+        ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+        bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+        ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+        bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+        ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+        bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+        ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+        bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+        ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+        bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+        ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+        bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+        ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+        bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+        ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+        bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+        ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+        bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+        ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+        bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+        ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+        bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+        ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+        bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+        ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+        bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+        ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+        bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+        ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+        bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+        ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+        bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+        ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+        fffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+        `)
+    myMenu = miniMenu.createMenu(
+    miniMenu.createMenuItem("Start Game"),
+    miniMenu.createMenuItem("Controls"),
+    miniMenu.createMenuItem("Credits")
+    )
+    myMenu.setStyleProperty(miniMenu.StyleKind.Selected, miniMenu.StyleProperty.Padding, 6)
+    myMenu.setStyleProperty(miniMenu.StyleKind.Selected, miniMenu.StyleProperty.Background, 2)
+    myMenu.setDimensions(100, 100)
+    myMenu.onButtonPressed(controller.A, function (selection, selectedIndex) {
+        if (selection == "Ur Gay") {
+            myMenu.close()
+        } else if (selection == "Controls") {
+            myMenu.close()
+        } else if (selection == "Credits") {
+            myMenu.close()
+        }
+    })
+}
+let myMenu: miniMenu.MenuSprite = null
+Start()
